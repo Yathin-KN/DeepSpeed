452bf95fc7ab84b347f4f0845855fc1630507887
Yathin-KN <yathin2184@gmail.com>
2024-06-01 18:06:01 +0530

    support for .dll and .so file

--- csrc/aio/py_lib/py_ds_aio_trampoline.cpp
+++ csrc/aio/py_lib/py_ds_aio_trampoline.cpp
@@ -1,44 +1,70 @@
+#include <dlfcn.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "deepspeed_aio_base.h"
-#include <string>
+#include <filesystem>
 #include <iostream>
 #include <memory>
-#include <dlfcn.h>
-#include <filesystem>
+#include <string>
+#include "deepspeed_aio_base.h"
 
 namespace py = pybind11;
 
 class DeepSpeedAIOTrampoline : public DeepSpeedAIOBase {
 public:
-    DeepSpeedAIOTrampoline(const std::string& device_type) : device(nullptr), handle(nullptr) {
+    DeepSpeedAIOTrampoline(const std::string& device_type) : device(nullptr), handle(nullptr)
+    {
         load_device(device_type);
     }
 
-    void load_device(const std::string& device_type) {
+    void load_device(const std::string& device_type)
+    {
         if (device) {
             delete device;
+            device = nullptr;
         }
 
         if (handle) {
+#if defined(_WIN32) || defined(_WIN64)
+            FreeLibrary(handle);
+#else
             dlclose(handle);
+#endif
+            handle = nullptr;
         }
 
-        std::filesystem::path so_directory = std::filesystem::current_path() / "deepspeed" / "ops" / "plugins";
+        std::filesystem::path so_directory =
+            std::filesystem::current_path() / "deepspeed" / "ops" / "plugins";
+        std::filesystem::path lib_path = so_directory / (device_type + "_op");
 
-        std::filesystem::path lib_path = so_directory / (device_type + "_op.so");
-
-        handle = dlopen(lib_name.c_str(), RTLD_LAZY);
+#if defined(_WIN32) || defined(_WIN64)
+        lib_path.replace_extension(".dll");
+        handle = LoadLibrary(lib_path.c_str());
+        if (!handle) {
+            std::cerr << "Cannot open library: " << GetLastError() << '\n';
+            return;
+        }
+#else
+        lib_path.replace_extension(".so");
+        handle = dlopen(lib_path.c_str(), RTLD_LAZY);
         if (!handle) {
             std::cerr << "Cannot open library: " << dlerror() << '\n';
             return;
         }
-
-        dlerror();
+        dlerror();  // Clear any existing error
+#endif
 
         typedef DeepSpeedAIOBase* (*create_t)();
 
-        create_t create_device = (create_t) dlsym(handle, "create_device");
+#if defined(_WIN32) || defined(_WIN64)
+        create_t create_device = (create_t)GetProcAddress(handle, "create_device");
+        if (!create_device) {
+            std::cerr << "Cannot load symbol create_device: " << GetLastError() << '\n';
+            FreeLibrary(handle);
+            handle = nullptr;
+            return;
+        }
+#else
+        create_t create_device = (create_t)dlsym(handle, "create_device");
         const char* dlsym_error = dlerror();
         if (dlsym_error) {
             std::cerr << "Cannot load symbol create_device: " << dlsym_error << '\n';
@@ -46,37 +72,79 @@ public:
             handle = nullptr;
             return;
         }
+#endif
 
         device = create_device();
     }
-
-    void aio_read(torch::Tensor& buffer, const char* filename, const bool validate) override { device->aio_read(buffer, filename, validate); }
-    void aio_write(const torch::Tensor& buffer, const char* filename, const bool validate) override { device->aio_write(buffer, filename, validate); }
-    void deepspeed_memcpy(torch::Tensor& dest, const torch::Tensor& src) override { device->deepspeed_memcpy(dest, src); }
+    void aio_read(torch::Tensor& buffer, const char* filename, const bool validate) override
+    {
+        device->aio_read(buffer, filename, validate);
+    }
+    void aio_write(const torch::Tensor& buffer, const char* filename, const bool validate) override
+    {
+        device->aio_write(buffer, filename, validate);
+    }
+    void deepspeed_memcpy(torch::Tensor& dest, const torch::Tensor& src) override
+    {
+        device->deepspeed_memcpy(dest, src);
+    }
     int get_block_size() const override { return device->get_block_size(); }
     int get_queue_depth() const override { return device->get_queue_depth(); }
     bool get_single_submit() const override { return device->get_single_submit(); }
     bool get_overlap_events() const override { return device->get_overlap_events(); }
     int get_thread_count() const override { return device->get_thread_count(); }
-    void read(torch::Tensor& buffer, const char* filename, const bool validate) override { device->read(buffer, filename, validate); }
-    void write(const torch::Tensor& buffer, const char* filename, const bool validate) override { device->write(buffer, filename, validate); }
-    void pread(const torch::Tensor& buffer, const char* filename, const bool validate, const bool async) override { device->pread(buffer, filename, validate, async); }
-    void pwrite(const torch::Tensor& buffer, const char* filename, const bool validate, const bool async) override { device->pwrite(buffer, filename, validate, async); }
-    void sync_pread(torch::Tensor& buffer, const char* filename) override { device->sync_pread(buffer, filename); }
-    void sync_pwrite(const torch::Tensor& buffer, const char* filename) override { device->sync_pwrite(buffer, filename); }
-    void async_pread(torch::Tensor& buffer, const char* filename) override { device->async_pread(buffer, filename); }
-    void async_pwrite(const torch::Tensor& buffer, const char* filename) override { device->async_pwrite(buffer, filename); }
-    void new_cpu_locked_tensor(const size_t num_elem, const torch::Tensor& example_tensor) override { device->new_cpu_locked_tensor(num_elem, example_tensor); }
-    void free_cpu_locked_tensor(torch::Tensor& tensor) override { device->free_cpu_locked_tensor(tensor); }
+    void read(torch::Tensor& buffer, const char* filename, const bool validate) override
+    {
+        device->read(buffer, filename, validate);
+    }
+    void write(const torch::Tensor& buffer, const char* filename, const bool validate) override
+    {
+        device->write(buffer, filename, validate);
+    }
+    void pread(const torch::Tensor& buffer,
+               const char* filename,
+               const bool validate,
+               const bool async) override
+    {
+        device->pread(buffer, filename, validate, async);
+    }
+    void pwrite(const torch::Tensor& buffer,
+                const char* filename,
+                const bool validate,
+                const bool async) override
+    {
+        device->pwrite(buffer, filename, validate, async);
+    }
+    void sync_pread(torch::Tensor& buffer, const char* filename) override
+    {
+        device->sync_pread(buffer, filename);
+    }
+    void sync_pwrite(const torch::Tensor& buffer, const char* filename) override
+    {
+        device->sync_pwrite(buffer, filename);
+    }
+    void async_pread(torch::Tensor& buffer, const char* filename) override
+    {
+        device->async_pread(buffer, filename);
+    }
+    void async_pwrite(const torch::Tensor& buffer, const char* filename) override
+    {
+        device->async_pwrite(buffer, filename);
+    }
+    void new_cpu_locked_tensor(const size_t num_elem, const torch::Tensor& example_tensor) override
+    {
+        device->new_cpu_locked_tensor(num_elem, example_tensor);
+    }
+    void free_cpu_locked_tensor(torch::Tensor& tensor) override
+    {
+        device->free_cpu_locked_tensor(tensor);
+    }
     void wait() override { device->wait(); }
 
-    ~DeepSpeedAIOTrampoline() {
-        if (device) {
-            delete device;
-        }
-        if (handle) {
-            dlclose(handle);
-        }
+    ~DeepSpeedAIOTrampoline()
+    {
+        if (device) { delete device; }
+        if (handle) { dlclose(handle); }
     }
 
 private:
@@ -84,13 +152,22 @@ private:
     void* handle;
 };
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
     py::class_<DeepSpeedAIOTrampoline>(m, "DeepSpeedAIOTrampoline")
         .def(py::init<const std::string&>(), py::arg("device_type") = "nvme")
         .def(py::init<const std::string&>())
         .def("load_device", &DeepSpeedAIOTrampoline::load_device)
-        .def("aio_read", &DeepSpeedAIOTrampoline::aio_read, py::arg("buffer"), py::arg("filename"), py::arg("validate") = true)
-        .def("aio_write", &DeepSpeedAIOTrampoline::aio_write, py::arg("buffer"), py::arg("filename"), py::arg("validate") = true)
+        .def("aio_read",
+             &DeepSpeedAIOTrampoline::aio_read,
+             py::arg("buffer"),
+             py::arg("filename"),
+             py::arg("validate") = true)
+        .def("aio_write",
+             &DeepSpeedAIOTrampoline::aio_write,
+             py::arg("buffer"),
+             py::arg("filename"),
+             py::arg("validate") = true)
         .def("deepspeed_memcpy", &DeepSpeedAIOTrampoline::deepspeed_memcpy)
         .def("get_block_size", &DeepSpeedAIOTrampoline::get_block_size)
         .def("get_queue_depth", &DeepSpeedAIOTrampoline::get_queue_depth)
@@ -99,35 +176,40 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
         .def("get_thread_count", &DeepSpeedAIOTrampoline::get_thread_count)
         .def("read", &DeepSpeedAIOTrampoline::read)
         .def("write", &DeepSpeedAIOTrampoline::write)
-        .def("pread", &DeepSpeedAIOTrampoline::pread, py::arg("buffer"), py::arg("filename"), py::arg("validate") = true, py::arg("async") = false)
-        .def("pwrite", &DeepSpeedAIOTrampoline::pwrite, py::arg("buffer"), py::arg("filename"), py::arg("validate") = true, py::arg("async") = false)
-        .def("sync_pread", &DeepSpeedAIOTrampoline::sync_pread, py::arg("buffer"), py::arg("filename"))
-        .def("sync_pwrite", &DeepSpeedAIOTrampoline::sync_pwrite, py::arg("buffer"), py::arg("filename"))
-        .def("async_pread", &DeepSpeedAIOTrampoline::async_pread, py::arg("buffer"), py::arg("filename"))
-        .def("async_pwrite", &DeepSpeedAIOTrampoline::async_pwrite, py::arg("buffer"), py::arg("filename"))
-        .def("new_cpu_locked_tensor", &DeepSpeedAIOTrampoline::new_cpu_locked_tensor, py::arg("num_elem"), py::arg("example_tensor"))
-        .def("free_cpu_locked_tensor", &DeepSpeedAIOTrampoline::free_cpu_locked_tensor, py::arg("tensor"))
+        .def("pread",
+             &DeepSpeedAIOTrampoline::pread,
+             py::arg("buffer"),
+             py::arg("filename"),
+             py::arg("validate") = true,
+             py::arg("async") = false)
+        .def("pwrite",
+             &DeepSpeedAIOTrampoline::pwrite,
+             py::arg("buffer"),
+             py::arg("filename"),
+             py::arg("validate") = true,
+             py::arg("async") = false)
+        .def("sync_pread",
+             &DeepSpeedAIOTrampoline::sync_pread,
+             py::arg("buffer"),
+             py::arg("filename"))
+        .def("sync_pwrite",
+             &DeepSpeedAIOTrampoline::sync_pwrite,
+             py::arg("buffer"),
+             py::arg("filename"))
+        .def("async_pread",
+             &DeepSpeedAIOTrampoline::async_pread,
+             py::arg("buffer"),
+             py::arg("filename"))
+        .def("async_pwrite",
+             &DeepSpeedAIOTrampoline::async_pwrite,
+             py::arg("buffer"),
+             py::arg("filename"))
+        .def("new_cpu_locked_tensor",
+             &DeepSpeedAIOTrampoline::new_cpu_locked_tensor,
+             py::arg("num_elem"),
+             py::arg("example_tensor"))
+        .def("free_cpu_locked_tensor",
+             &DeepSpeedAIOTrampoline::free_cpu_locked_tensor,
+             py::arg("tensor"))
         .def("wait", &DeepSpeedAIOTrampoline::wait);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- csrc/aio/py_lib/trampoline.h
+++ csrc/aio/py_lib/trampoline.h
@@ -1,28 +1,26 @@
 #pragma once
 
-#include <string>
-#include <memory>
 #include <dlfcn.h>
 #include <iostream>
+#include <memory>
+#include <string>
 #include "../include/deepspeed_aio_base.h"
-#include <filesystem>
 
 class Trampoline {
 public:
-    explicit Trampoline(const std::string& device_type) : device(nullptr), handle(nullptr) {
+    explicit Trampoline(const std::string& device_type) : device(nullptr), handle(nullptr)
+    {
         load_device(device_type);
     }
 
-    void load_device(const std::string& device_type) {
-        if (device) {
-            delete device;
-        }
+    void load_device(const std::string& device_type)
+    {
+        if (device) { delete device; }
 
-        if (handle) {
-            dlclose(handle);
-        }
+        if (handle) { dlclose(handle); }
 
-        std::filesystem::path so_directory = std::filesystem::current_path() / "deepspeed" / "ops" / "plugins";
+        std::filesystem::path so_directory =
+            std::filesystem::current_path() / "deepspeed" / "ops" / "plugins";
 
         std::filesystem::path lib_path = so_directory / (device_type + "_op.so");
 
@@ -35,7 +33,7 @@ public:
         dlerror();
 
         typedef DeepSpeedAIOBase* (*create_t)();
-        create_t create_device = (create_t) dlsym(handle, "create_device");
+        create_t create_device = (create_t)dlsym(handle, "create_device");
         const char* dlsym_error = dlerror();
         if (dlsym_error) {
             std::cerr << "Cannot load symbol create_device: " << dlsym_error << '\n';
@@ -47,38 +45,171 @@ public:
         device = create_device();
     }
 
-    void aio_read(torch::Tensor& buffer, const char* filename, const bool validate) { if (device) device->aio_read(buffer, filename, validate); else std::cerr << "No device loaded for aio_read\n"; }
-    void aio_write(const torch::Tensor& buffer, const char* filename, const bool validate) { if (device) device->aio_write(buffer, filename, validate); else std::cerr << "No device loaded for aio_write\n"; }
-    void deepspeed_memcpy(torch::Tensor& dest, const torch::Tensor& src) { if (device) device->deepspeed_memcpy(dest, src); else std::cerr << "No device loaded for deepspeed_memcpy\n"; }
+    void aio_read(torch::Tensor& buffer, const char* filename, const bool validate)
+    {
+        if (device)
+            device->aio_read(buffer, filename, validate);
+        else
+            std::cerr << "No device loaded for aio_read\n";
+    }
+    void aio_write(const torch::Tensor& buffer, const char* filename, const bool validate)
+    {
+        if (device)
+            device->aio_write(buffer, filename, validate);
+        else
+            std::cerr << "No device loaded for aio_write\n";
+    }
+    void deepspeed_memcpy(torch::Tensor& dest, const torch::Tensor& src)
+    {
+        if (device)
+            device->deepspeed_memcpy(dest, src);
+        else
+            std::cerr << "No device loaded for deepspeed_memcpy\n";
+    }
 
-    int get_block_size() const { if (device) return device->get_block_size(); else { std::cerr << "No device loaded for get_block_size\n"; return -1; } }
-    int get_queue_depth() const { if (device) return device->get_queue_depth(); else { std::cerr << "No device loaded for get_queue_depth\n"; return -1; } }
-    bool get_single_submit() const { if (device) return device->get_single_submit(); else { std::cerr << "No device loaded for get_single_submit\n"; return false; } }
-    bool get_overlap_events() const { if (device) return device->get_overlap_events(); else { std::cerr << "No device loaded for get_overlap_events\n"; return false; } }
-    int get_thread_count() const { if (device) return device->get_thread_count(); else { std::cerr << "No device loaded for get_thread_count\n"; return -1; } }
-
-    void read(torch::Tensor& buffer, const char* filename, const bool validate) { if (device) device->read(buffer, filename, validate); else std::cerr << "No device loaded for read\n"; }
-    void write(const torch::Tensor& buffer, const char* filename, const bool validate) { if (device) device->write(buffer, filename, validate); else std::cerr << "No device loaded for write\n"; }
-    void pread(const torch::Tensor& buffer, const char* filename, const bool validate, const bool async) { if (device) device->pread(buffer, filename, validate, async); else std::cerr << "No device loaded for pread\n"; }
-    void pwrite(const torch::Tensor& buffer, const char* filename, const bool validate, const bool async) { if (device) device->pwrite(buffer, filename, validate, async); else std::cerr << "No device loaded for pwrite\n"; }
-
-    void sync_pread(torch::Tensor& buffer, const char* filename) { if (device) device->sync_pread(buffer, filename); else std::cerr << "No device loaded for sync_pread\n"; }
-    void sync_pwrite(const torch::Tensor& buffer, const char* filename) { if (device) device->sync_pwrite(buffer, filename); else std::cerr << "No device loaded for sync_pwrite\n"; }
-    void async_pread(torch::Tensor& buffer, const char* filename) { if (device) device->async_pread(buffer, filename); else std::cerr << "No device loaded for async_pread\n"; }
-    void async_pwrite(const torch::Tensor& buffer, const char* filename) { if (device) device->async_pwrite(buffer, filename); else std::cerr << "No device loaded for async_pwrite\n"; }
-
-    void new_cpu_locked_tensor(const size_t num_elem, const torch::Tensor& example_tensor) { if (device) device->new_cpu_locked_tensor(num_elem, example_tensor); else std::cerr << "No device loaded for new_cpu_locked_tensor\n"; }
-    void free_cpu_locked_tensor(torch::Tensor& tensor) { if (device) device->free_cpu_locked_tensor(tensor); else std::cerr << "No device loaded for free_cpu_locked_tensor\n"; }
-
-    void wait() { if (device) device->wait(); else std::cerr << "No device loaded for wait\n"; }
-
-    ~Trampoline() {
-        if (device) {
-            delete device;
+    int get_block_size() const
+    {
+        if (device)
+            return device->get_block_size();
+        else {
+            std::cerr << "No device loaded for get_block_size\n";
+            return -1;
         }
-        if (handle) {
-            dlclose(handle);
+    }
+    int get_queue_depth() const
+    {
+        if (device)
+            return device->get_queue_depth();
+        else {
+            std::cerr << "No device loaded for get_queue_depth\n";
+            return -1;
         }
+    }
+    bool get_single_submit() const
+    {
+        if (device)
+            return device->get_single_submit();
+        else {
+            std::cerr << "No device loaded for get_single_submit\n";
+            return false;
+        }
+    }
+    bool get_overlap_events() const
+    {
+        if (device)
+            return device->get_overlap_events();
+        else {
+            std::cerr << "No device loaded for get_overlap_events\n";
+            return false;
+        }
+    }
+    int get_thread_count() const
+    {
+        if (device)
+            return device->get_thread_count();
+        else {
+            std::cerr << "No device loaded for get_thread_count\n";
+            return -1;
+        }
+    }
+
+    void read(torch::Tensor& buffer, const char* filename, const bool validate)
+    {
+        if (device)
+            device->read(buffer, filename, validate);
+        else
+            std::cerr << "No device loaded for read\n";
+    }
+    void write(const torch::Tensor& buffer, const char* filename, const bool validate)
+    {
+        if (device)
+            device->write(buffer, filename, validate);
+        else
+            std::cerr << "No device loaded for write\n";
+    }
+    void pread(const torch::Tensor& buffer,
+               const char* filename,
+               const bool validate,
+               const bool async)
+    {
+        if (device)
+            device->pread(buffer, filename, validate, async);
+        else
+            std::cerr << "No device loaded for pread\n";
+    }
+    void pwrite(const torch::Tensor& buffer,
+                const char* filename,
+                const bool validate,
+                const bool async)
+    {
+        if (device)
+            device->pwrite(buffer, filename, validate, async);
+        else
+            std::cerr << "No device loaded for pwrite\n";
+    }
+
+    void sync_pread(torch::Tensor& buffer, const char* filename)
+    {
+        if (device)
+            device->sync_pread(buffer, filename);
+        else
+            std::cerr << "No device loaded for sync_pread\n";
+    }
+    void sync_pwrite(const torch::Tensor& buffer, const char* filename)
+    {
+        if (device)
+            device->sync_pwrite(buffer, filename);
+        else
+            std::cerr << "No device loaded for sync_pwrite\n";
+    }
+    void async_pread(torch::Tensor& buffer, const char* filename)
+    {
+        if (device)
+            device->async_pread(buffer, filename);
+        else
+            std::cerr << "No device loaded for async_pread\n";
+    }
+    void async_pwrite(const torch::Tensor& buffer, const char* filename)
+    {
+        if (device)
+            device->async_pwrite(buffer, filename);
+        else
+            std::cerr << "No device loaded for async_pwrite\n";
+    }
+
+    void new_cpu_locked_tensor(const size_t num_elem, const torch::Tensor& example_tensor)
+    {
+        if (device)
+            device->new_cpu_locked_tensor(num_elem, example_tensor);
+        else
+            std::cerr << "No device loaded for new_cpu_locked_tensor\n";
+    }
+    void free_cpu_locked_tensor(torch::Tensor& tensor)
+    {
+        if (device)
+            device->free_cpu_locked_tensor(tensor);
+        else
+            std::cerr << "No device loaded for free_cpu_locked_tensor\n";
+    }
+
+    void wait()
+    {
+        if (device)
+            device->wait();
+        else
+            std::cerr << "No device loaded for wait\n";
+    }
+
+    ~Trampoline()
+    {
+        if (device) { delete device; }
+        if (handle) { dlclose(handle); }
+    }
+
+private:
+    DeepSpeedAIOBase* device;
+    void* handle;
+};
     }
 
 private:
